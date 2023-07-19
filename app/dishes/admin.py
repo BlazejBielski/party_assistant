b2d030ac868db0efac20efa62f5ddf8ed97371d6
Blazej Bielski <bbielski@lyck.pl>
2023-07-19 21:47:19 +0200

    extended model in admin, give a new default value for dish mark

--- app/dishes/admin.py
+++ app/dishes/admin.py
@@ -4,9 +4,9 @@ from dishes.models import Soup
 
 
 class SoupAdminConfig(admin.ModelAdmin):
-    list_display = ('id', 'name', 'description', 'price', 'slug')
+    list_display = ('id', 'name', 'price', 'slug', 'created_at')
     search_fields = ('name',)
-    list_filter = ('name', 'created_at', 'updated_at')
+    list_filter = ('name', 'created_at', 'updated_at', 'is_vegetarian', 'is_keto', 'is_gluten_free', 'is_low_carb')
 
 
-admin.site.register(Soup)
+admin.site.register(Soup, SoupAdminConfig)
--- app/utilities/dishmark_model.py
+++ app/utilities/dishmark_model.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class DishMarkModel(models.Model):
-    is_vegetarian = models.BooleanField(default=False, blank=True, null=True)
-    is_gluten_free = models.BooleanField(default=False, blank=True, null=True)
-    is_low_carb = models.BooleanField(default=False, blank=True, null=True)
-    is_keto = models.BooleanField(default=False, blank=True, null=True)
+    is_vegetarian = models.BooleanField(default=None, blank=True, null=True)
+    is_gluten_free = models.BooleanField(default=None, blank=True, null=True)
+    is_low_carb = models.BooleanField(default=None, blank=True, null=True)
+    is_keto = models.BooleanField(default=None, blank=True, null=True)
 
     class Meta:
         abstract = True
