3f5ee5895aa3719f476ae75cc05e27b59fcf7c2b
Blazej Bielski <bbielski@lyck.pl>
2023-07-19 21:52:35 +0200

    model main dish

--- app/dishes/models.py
+++ app/dishes/models.py
@@ -26,6 +26,26 @@ class Soup(TimeStampModel, DishMarkModel):
         verbose_name_plural = "Soups"
 
 
+class MainDish(TimeStampModel, DishMarkModel):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    slug = models.SlugField(blank=True, max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Main Dish'
+        verbose_name_plural = 'Main Dishes'
+
+
 class Picture(TimeStampModel):
     soup = models.ForeignKey('Soup', on_delete=models.CASCADE, related_name='soup_image')
     image = models.FileField(upload_to='images/', validators=[validate_file_type])
