8b96a180623e0d5962b3e424ff85dbb115bde5b5
Blazej Bielski <bbielski@lyck.pl>
2023-07-19 18:42:14 +0200

    fix typo in slug field

--- app/dishes/models.py
+++ app/dishes/models.py
@@ -9,7 +9,7 @@ class Soup(TimeStampModel):
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    slug = models = models.SlugField(blank=True, max_length=50, unique=True)
+    slug = models.SlugField(blank=True, max_length=50, unique=True)
 
     def __str__(self):
         return self.name
