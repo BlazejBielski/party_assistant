22a1c963cff28987125bbf4229555264cd9d8ee8
Blazej Bielski <bbielski@lyck.pl>
2023-07-22 11:52:49 +0200

    clenaup models file and register all class in admin

--- app/dishes/admin.py
+++ app/dishes/admin.py
@@ -3,11 +3,33 @@ from django.contrib import admin
 from . import models
 
 
-class SoupAdminConfig(admin.ModelAdmin):
+class DishAdminConfig(admin.ModelAdmin):
     model = models.Soup
     list_display = ('id', 'name', 'price', 'slug', 'created_at')
     search_fields = ('name',)
     list_filter = ('name', 'created_at', 'updated_at', 'is_vegetarian', 'is_keto', 'is_gluten_free', 'is_low_carb')
 
+    class Meta:
+        abstract = True
+
+
+class SoupAdminConfig(DishAdminConfig):
+    pass
+
+
+class MainDishAdminConfig(DishAdminConfig):
+    pass
+
+
+class AppetizerAdminConfig(DishAdminConfig):
+    pass
+
+
+class SideDishAdminConfig(DishAdminConfig):
+    pass
+
 
 admin.site.register(models.Soup, SoupAdminConfig)
+admin.site.register(models.MainDish, MainDishAdminConfig)
+admin.site.register(models.Appetizer, AppetizerAdminConfig)
+admin.site.register(models.SideDish, SideDishAdminConfig)
--- app/dishes/models.py
+++ app/dishes/models.py
@@ -24,16 +24,8 @@ class Appetizer(TimeStampModel, DishMarkModel, DishCoreModel):
         verbose_name_plural = 'Appetizers'
 
 
-class SideDishes(TimeStampModel, DishMarkModel, DishCoreModel):
+class SideDish(TimeStampModel, DishMarkModel, DishCoreModel):
 
     class Meta:
         verbose_name = 'Side dish'
         verbose_name_plural = 'Side dishes'
-
-
-# class Picture(TimeStampModel):
-#     soup = models.ForeignKey('Soup', on_delete=models.CASCADE, related_name='soup_image')
-#     main_dish = models.ForeignKey('MainDish', on_delete=models.CASCADE, related_name='main_dish_image')
-#     appetizer = models.ForeignKey('Appetizer', on_delete=models.CASCADE, related_name='appetizer_image')
-#     side_dish = models.ForeignKey('SideDishes', on_delete=models.CASCADE, related_name='side_dish_image')
-#     image = models.FileField(upload_to='images/', validators=[validate_file_type])
